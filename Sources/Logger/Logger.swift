fd496b49c6833bafc5c9535e8492c8561d092fe8
AlexeyOkhin <k-i-ll-e-r@list.ru>
2024-01-03 20:04:02 +0500

    add logger

--- Sources/Logger/Logger.swift
+++ Sources/Logger/Logger.swift
@@ -1,2 +1,52 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Logger.swift
+//
+//  Created by Djinsolobzik on 20.02.2023.
+//
+
+import Foundation
+
+enum Logger {
+    enum LogLevel: String {
+        case info
+        case error
+
+        fileprivate var prefix: String {
+            switch self {
+            case .info: return "📗 INFO"
+            case .error: return "📕 ERORR"
+            }
+        }
+    }
+
+    struct Context {
+        let function: String
+        var description: String {
+            return "\(function)"
+        }
+    }
+
+    static func info(_ message: String, shouldLogContext: Bool = true, function: String = #function) {
+        let context = Context(function: function)
+        Logger.handleLog(level: .info, message: message.description, shouldLogContext: shouldLogContext, context: context)
+    }
+
+    static func error(_ message: String, shouldLogContext: Bool = true, function: String = #function) {
+        let context = Context(function: function)
+        Logger.handleLog(level: .error, message: message.description, shouldLogContext: shouldLogContext, context: context)
+    }
+
+    fileprivate static func handleLog(level: LogLevel, message: String, shouldLogContext: Bool, context: Context) {
+
+        let logComponents = ["[\(level.prefix)]", message]
+
+        var fullString = logComponents.joined(separator: " ")
+        if shouldLogContext {
+            fullString += " -> \(context.description)"
+        }
+
+        #if DEBUG
+        print(fullString)
+        #endif
+    }
+}
